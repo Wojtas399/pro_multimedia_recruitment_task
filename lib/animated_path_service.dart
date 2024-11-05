65736c5276faf15ebba95c79168745322cd98c3b
Wojtas399 <wojtekp26@gmail.com>
2024-11-05 22:04:05 +0100

    Created service for path animation.

---
+++ lib/animated_path_service.dart
@@ -0,0 +1,50 @@
+import 'dart:ui';
+
+class AnimatedPathService {
+  Path createAnimatedPath(
+    Path originalPath,
+    double animationPercent,
+  ) {
+    final totalLength = originalPath
+        .computeMetrics()
+        .fold(0.0, (double prev, PathMetric metric) => prev + metric.length);
+
+    final currentLength = totalLength * animationPercent;
+
+    return _extractPathUntilLength(originalPath, currentLength);
+  }
+
+  Path _extractPathUntilLength(
+    Path originalPath,
+    double length,
+  ) {
+    var currentLength = 0.0;
+
+    final path = Path();
+
+    var metricsIterator = originalPath.computeMetrics().iterator;
+
+    while (metricsIterator.moveNext()) {
+      var metric = metricsIterator.current;
+
+      var nextLength = currentLength + metric.length;
+
+      final isLastSegment = nextLength > length;
+      if (isLastSegment) {
+        final remainingLength = length - currentLength;
+        final pathSegment = metric.extractPath(0.0, remainingLength);
+
+        path.addPath(pathSegment, Offset.zero);
+        break;
+      } else {
+        // There might be a more efficient way of extracting an entire path
+        final pathSegment = metric.extractPath(0.0, metric.length);
+        path.addPath(pathSegment, Offset.zero);
+      }
+
+      currentLength = nextLength;
+    }
+
+    return path;
+  }
+}
